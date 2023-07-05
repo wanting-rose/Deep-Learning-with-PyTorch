eb26f7d82a9179cf7cc7f8889e1ae64aedb903aa
wanting-rose <hewantin@usc.edu>
2023-07-05 18:00:04 -0400

    This is project in initial state.

--- 02_linear_regression.py
+++ 02_linear_regression.py
@@ -1,8 +1,56 @@
-# design model (input size, output size, forward pass)
-# construct loss and optimizer
-# training loop
+# 1) design model (input size, output size, forward pass)
+# 2) construct loss and optimizer
+# 3) training loop
 #       - Forward = compute prediction and loss
 #       - Backward = compute gradients
 #       - Update weights
+
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# prepare data
+X_numpy, Y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=1)
+X = torch.from_numpy(X_numpy.astype(np.float32))
+Y = torch.from_numpy(Y_numpy.astype(np.float32))
+Y = Y.view(Y.shape[0],1)
+
+n_samples, n_features = X.shape
+# model
+input_size = n_features
+output_size = 1
+
+model = nn.Linear(input_size, output_size)
+
+# loss and optimizer
+learning_rate = 0.01
+criterion = nn.MSELoss()   # mean square error
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# training loop
+num_epochs = 100
+for epochs in range(num_epochs):
+    # forward pass and loss
+    y_predicted = model(X)
+    loss = criterion(y_predicted,Y)
+
+    # backward pass
+    loss.backward()
+
+    #update
+    optimizer.step()
+
+    optimizer.zero_grad()
+
+    if (epochs+1)%10 == 0:
+        print(f'epoch:{epochs+1}, loss = {loss.item():.4f}')
+
+# plot
+predicted = model(X).detach().numpy()
+plt.plot(X_numpy, Y_numpy, 'ro')
+plt.plot(X_numpy, predicted, 'b')
+plt.show()
 
 
